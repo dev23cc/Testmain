7fc39eece9d68bfcf005c630c623c2a15cf96371
dev23cc <dev23cc@yandex.com>
2015-11-28 21:06:52 +0200

    nees allages

---  test1arxiko/src/test1arxiko/Test1arxiko.java
+++  test1arxiko/src/test1arxiko/Test1arxiko.java
@@ -15,7 +15,7 @@ public class Test1arxiko {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(System.getProperty("os.name"));
+        System.out.println("os name "+ System.getProperty("os.name"));
         // TODO code application logic here
     }
     
